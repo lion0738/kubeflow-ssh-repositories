52b8a697c4cb2be212c2ee75e2a3397352c4b89b
Lee Sang Hwa <lion0738@naver.com>
2025-05-26 17:55:27 +0900

    경로 변경 및 minio, s3fs 추가

--- jupyter-cuda/deploy_images.sh
+++ jupyter-cuda/deploy_images.sh
@@ -10,7 +10,7 @@ images=(
 for image in "${images[@]}"; do
     buildah bud \
         --build-arg BASE_IMG=ghcr.io/kubeflow/kubeflow/notebook-servers/$image:v1.10.0 \
-        --tag docker.io/lion0738/${image}-ssh:v1.10.0 \
+        --tag ghcr.io/lion0738/${image}-ssh:v1.10.0 \
         -f Dockerfile .
-    buildah push docker.io/lion0738/${image}-ssh:v1.10.0
+    buildah push ghcr.io/lion0738/${image}-ssh:v1.10.0
 done
