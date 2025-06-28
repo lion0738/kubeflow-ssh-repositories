55a584fee209543418ce0a1d69bc408019193ed7
Lee Sang Hwa <lion0738@naver.com>
2025-06-28 21:53:26 +0900

    이미지 경로 업데이트

--- jupyter-cuda/deploy_images.sh
+++ jupyter-cuda/deploy_images.sh
@@ -10,7 +10,7 @@ images=(
 for image in "${images[@]}"; do
     buildah bud \
         --build-arg BASE_IMG=ghcr.io/kubeflow/kubeflow/notebook-servers/$image:v1.10.0 \
-        --tag knu-kubeflow.duckdns.org:30500/kubeflow-images/${image}-ssh:v1.10.0 \
+        --tag harbor.knu-kubeflow.duckdns.org/kubeflow-images/${image}-ssh:v1.10.0 \
         -f Dockerfile .
-    buildah push knu-kubeflow.duckdns.org:30500/kubeflow-images/${image}-ssh:v1.10.0
+    buildah push harbor.knu-kubeflow.duckdns.org/kubeflow-images/${image}-ssh:v1.10.0
 done
